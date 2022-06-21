2b2f8f349e9f5c61f7df626bb383c41313cb7d0f
RolandTapie <kenloth1@yahoo.fr>
2022-06-21 12:32:54 +0200

    1 binaries uploaded

--- src/app/module/module.component.ts
+++ src/app/module/module.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ModuleComponent implements OnInit {
   @Input()
   nom: string = 'vide';
+  img: string ="img.png"
   constructor() {}
 
   ngOnInit() {}
